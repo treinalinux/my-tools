891887fb20f0d977cff91643a5fad35cddd91c1b
Alan da Silva Alves <linuxfama@gmail.com>
2024-04-07 16:48:46 -0300

    feat: app add automount logs new 1.0.2

--- Linux/Ldap/Apps/Automount/add_automount.py
+++ Linux/Ldap/Apps/Automount/add_automount.py
@@ -3,7 +3,7 @@
 # name.........: add_automount
 # description..: Add new entry in automount
 # author.......: Alan da Silva Alves
-# version......: 0.0.1
+# version......: 1.0.2
 #
 # ---------------------------------------------------------------------------
 # ---------------------------------------------------------------------------
@@ -15,7 +15,8 @@ import os
 
 ANALYST = os.environ.get('USER')
 BASE_AUTOMOUNT = "OU=automount,OU=NIS,OU=UNIX,OU=POSIX,DC=EMPRESA,DC=CORP"
-
+LOGS = "/var/log/empresa/user_changes.log"
+APP = "add_automount"
 
 # ---------------------------------------------------------------------------
 # ---------------------------------------------------------------------------
@@ -37,6 +38,16 @@ nisMapName: {map_name}
 
     file_ldif = (f"/tmp/{ANALYST}-{account_ad}-file.ldif")
     append_msg_file(msg, file_ldif)
+
+
+def get_nis_map_name(nis_map, base_search):
+    ldap_search = "ldapsearch -Q -LLL -o ldif-wrap=no -b"
+    get_dn = (f"{ldap_search} '{base_search}' cn={nis_map} dn")
+    result = os.popen(get_dn).read()
+
+    if result == '':
+        print(f"\nO nisMapName: '{nis_map}' nao existe no AD!")
+        raise SystemExit("\nVerifique se o nome esta correto!\n")
 
 
 def get_dn_account_ad(account_ad, base_search):
@@ -65,7 +76,7 @@ def add_nis_map_entry(account_ad, map_name, nis_map_entry, value_entry):
     new_dn = (f"dn: CN={account_ad},CN={map_name},{BASE_AUTOMOUNT}")
     create_file_ldif(new_dn, account_ad, map_name, nis_map_entry, value_entry)
 
-    print(f"\nAdicionado entrada para '{account_ad}' em automount:")
+    print(f"\nAdicionando entrada para '{account_ad}' em {map_name}...")
 
     ldap_add(account_ad)
     get_account_ad(account_ad, BASE_AUTOMOUNT, nis_map_entry)
@@ -82,9 +93,22 @@ def ldap_add(account_ad):
     os.remove(file_ldif)
 
 
+def control_add(sn_code, manager, account_ad, map_name, attr, value_attr):
+    get_nis_map_name(map_name, BASE_AUTOMOUNT)
+    m_account = (f"manager={manager} account={account_ad}")
+    f_log = (f"{sn_code} \t{ANALYST} \t{APP} \t{m_account} \t{value_attr}")
+
+    add_nis_map_entry(account_ad, map_name, attr, value_attr)
+
+    log_action = (f"date \t{f_log}\n")
+    append_msg_file(log_action, LOGS)
+
+    print("")
+
+
 # ---------------------------------------------------------------------------
 # ---------------------------------------------------------------------------
+
 
 if __name__ == '__main__':
     print("Import o módulo para usar o recurso!")
-
